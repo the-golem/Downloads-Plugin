99b786a497aea017bd85aedca2260afaea28524d
Darrin C. Roenfanz <darrin.roenfanz@gmail.com>
2011-10-28 00:46:19 -0700

    Removed all php short-tags. Using "<?" and "<?=" caused display errors. Changed to "<?php" and "<?php echo " respectively.

--- views/index.php
+++ views/index.php
@@ -13,62 +13,61 @@ if (!defined('IN_CMS')) { exit(); }
 $baseURL = get_url('plugin/downloads') . '?q=' . (isset($_REQUEST['q']) ? $_REQUEST['q'] : '');
 $fullURL = $baseURL . (isset($_REQUEST['order']) ? '&order=' . $_REQUEST['order'] : '');
 
-$pagiation = '';
-if ($pages > 1) for ($i = 1; $i <= $pages; $i++) $pagiation .= '<a href="'.$fullURL.'&pg='.$i.'">'.($i==$page ? '<strong>'.$i.'</strong>' : $i ).'</a> ';
-if (!empty($pagiation)) $pagiation = '<div style="text-align:center;">'.($page != 1 ? '<span style="float:left;"><a href="'.$fullURL.'&pg=1">&lt;&lt;</a> <a href="'.$fullURL.'&pg='.($page-1).'">&lt;</a></span> ' : '').'<span class="pages">'.$pagiation.'</span>'.($page != $pages ? ' <span style="float:right;"><a href="'.$fullURL.'&pg='.($page+1).'">&gt;</a> <a href="'.$fullURL.'&pg='.$pages.'">&gt;&gt;</a></span>' : '').'</div>';
+$pagination = '';
+if ($pages > 1) for ($i = 1; $i <= $pages; $i++) $pagination .= '<a href="'.$fullURL.'&pg='.$i.'">'.($i==$page ? '<strong>'.$i.'</strong>' : $i ).'</a> ';
+if (!empty($pagination)) $pagination = '<div style="text-align:center;">'.($page != 1 ? '<span style="float:left;"><a href="'.$fullURL.'&pg=1">&lt;&lt;</a> <a href="'.$fullURL.'&pg='.($page-1).'">&lt;</a></span> ' : '').'<span class="pages">'.$pagination.'</span>'.($page != $pages ? ' <span style="float:right;"><a href="'.$fullURL.'&pg='.($page+1).'">&gt;</a> <a href="'.$fullURL.'&pg='.$pages.'">&gt;&gt;</a></span>' : '').'</div>';
 
 ?>
 <div style="float:right;margin:-10px;" class="noprint">
-	<form id="forms-search" action="<?=get_url('plugin/downloads'); ?>" method="get">
-		<input type="text" name="q" value="<?=isset($_REQUEST['q']) ? $_REQUEST['q'] : '';?>" /> <input type="submit" value="Search Forms" />
+	<form id="forms-search" action="<?php echo get_url('plugin/downloads'); ?>" method="get">
+		<input type="text" name="q" value="<?php echo isset($_REQUEST['q']) ? $_REQUEST['q'] : '';?>"> <input type="submit" value="Search Forms">
 	</form>
 </div>
 <h1>Download Manager</h1>
 <table id="files-list" class="index" cellpadding="0" cellspacing="0" border="0">
   <thead>
     <tr>
-      <th class="name"><a href="<?=$baseURL;?>&order=name"><?=__('Name'); ?></a></th>
-      <th class="id"><a href="<?=$baseURL;?>&order=id"><?=__('ID');?></a></th>
-      <th class="dcount"><a href="<?=$baseURL;?>&order=downloads"><?=__('Downloads'); ?></a></th>
-      <th class="created"><a href="<?=$baseURL;?>&order=created"><?=__('Created'); ?></a></th>
-      <th class="expires"><a href="<?=$baseURL;?>&order=expires"><?=__('Expires'); ?></a></th>
-      <th class="action" style="width:56px;"><?=__('Action');?></th>
+      <th class="name"><a href="<?php echo $baseURL;?>&order=name"><?php echo __('Name'); ?></a></th>
+      <th class="id"><a href="<?php echo $baseURL;?>&order=id"><?php echo __('ID');?></a></th>
+      <th class="dcount"><a href="<?php echo $baseURL;?>&order=downloads"><?php echo __('Downloads'); ?></a></th>
+      <th class="created"><a href="<?php echo $baseURL;?>&order=created"><?php echo __('Created'); ?></a></th>
+      <th class="expires"><a href="<?php echo $baseURL;?>&order=expires"><?php echo __('Expires'); ?></a></th>
+      <th class="action" style="width:56px;"><?php echo __('Action');?></th>
     </tr>
   </thead>
   <tbody>
-<?
+<?php
 foreach ($downloads as $download) {
 	$name = htmlspecialchars($download->name);
 ?>
-	<tr class="<?=odd_even(); ?>">
-		<td><a href="<?=get_url('plugin/downloads/edit/'.$download->id); ?>"><?=$name;?></a></td>
-		<td><code><?=$download->id;?></code></td>
-		<td><code><?=$download->downloads; ?></code></td>
-		<td><code><?=date('m/d/Y',strtotime($download->created)); ?></code></td>
-		<td><code><?=!empty($download->expires) ? date('m/d/Y',strtotime($download->expires)) : 'never'; ?></code></td>
+	<tr class="<?php echo odd_even(); ?>">
+		<td><a href="<?php echo get_url('plugin/downloads/edit/'.$download->id); ?>"><?php echo $name;?></a></td>
+		<td><code><?php echo $download->id;?></code></td>
+		<td><code><?php echo $download->downloads; ?></code></td>
+		<td><code><?php echo date('m/d/Y',strtotime($download->created)); ?></code></td>
+		<td><code><?php echo !empty($download->expires) ? date('m/d/Y',strtotime($download->expires)) : 'never'; ?></code></td>
 		<td>
-			<?="<a href=\"/download-file/{$download->id}/{$download->filename}\" target=\"_blank\" title=\"{$download->filename}\"><img src=\"".PLUGINS_URI."/downloads/images/icon-open.png\" alt=\"view icon\" title=\"View\"></a>"; ?>
-			<a class="edit-link" name="<?=$name;?>" href="<?=get_url('plugin/downloads/edit/'.$download->id); ?>"><img src="<?=PLUGINS_URI;?>/downloads/images/icon-edit.png" alt="edit icon" title="Edit <?=$name;?>" /></a>
-			<a class="delete-link" name="<?=$name;?>" href="<?=get_url('plugin/downloads/delete/'.$download->id); ?>"><img src="<?=PLUGINS_URI;?>/downloads/images/icon-trash.png" alt="delete file icon" title="Delete <?=$name;?>" /></a>
+			<a href="/download-file/<?php echo $download->id.'/'.$download->filename;?>" target="_blank" title="<?php echo $download->filename;?>"><img src="<?php echo PLUGINS_URI;?>/downloads/images/icon-open.png" alt="view icon" title="View"></a>
+			<a class="edit-link" name="<?php echo $name;?>" href="<?php echo get_url('plugin/downloads/edit/'.$download->id); ?>"><img src="<?php echo PLUGINS_URI;?>/downloads/images/icon-edit.png" alt="edit icon" title="Edit <?php echo $name;?>"></a>
+			<a class="delete-link" name="<?php echo $name;?>" href="<?php echo get_url('plugin/downloads/delete/'.$download->id); ?>"><img src="<?php echo PLUGINS_URI;?>/downloads/images/icon-trash.png" alt="delete file icon" title="Delete <?php echo $name;?>"></a>
 		</td>
 	</tr>
-<?
-}
-?>
+<?php } ?>
 </tbody>
 </table>
-<?=$pagiation;?>
+<?php echo $pagination;?>
+
 <script type="text/javascript">
-<!--
-$(function(){
+    <!--
+    $(function(){
 
-	$('.delete-link').click(function(e){
-		if (!confirm("Delete file "+$(this).attr('name')+"?\n\nAre you sure you want to delete this download?\nPress OK to delete this download permenently.")) {
-			e.preventDefault();
-		}
-		
-	});
+        $('.delete-link').click(function(e){
+            if (!confirm("Delete file "+$(this).attr('name')+"?\n\nAre you sure you want to delete this download?\nPress OK to delete this download permenently.")) {
+                e.preventDefault();
+            }
+            
+        });
 
-});
-//-->
+    }); 
+    //-->
 </script>
